9d7bafc486b8cd2aa41f7db73154783d8ec23340
gqbeerman <gqbeerman@yahoo.com>
2021-03-11 00:28:46 -0800

    math mod

--- script.js
+++ script.js
@@ -241,7 +241,7 @@ function createEquations() {
     secondNumber = getRandomInt(9);
     const equationValue = firstNumber * secondNumber;
     wrongFormat[0] = `${firstNumber} x ${secondNumber + 1} = ${equationValue}`;
-    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue - 1}`;
+    wrongFormat[1] = `${firstNumber} x ${secondNumber} = ${equationValue + 1}`;
     wrongFormat[2] = `${firstNumber + 1} x ${secondNumber} = ${equationValue}`;
     const formatChoice = getRandomInt(3);
     const equation = wrongFormat[formatChoice];
